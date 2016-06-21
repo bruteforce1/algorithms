17218c1fbef955b310905f37d33ede99bf4befc4
mattj <jabroney182@gmail.com>
2016-06-20 23:05:08 -0400

    Added random data generator for testing.

---
+++ Sorting/genrandforsorting.py
@@ -0,0 +1,40 @@
+"""@package genrandforsorting
+This module simply generates random numbers to use within the sorting 
+algorithms.
+"""
+
+import random
+import sys
+import time
+
+class genrandforsorting:
+    def __init__(self, 
+                 seed=1,
+                 minimum=0, 
+                 maximum=10000, 
+                 count=100, 
+                 filename='out.txt'):
+        """Initializes genrandforsorting class."""
+        self.seed = seed
+        self.min = minimum
+        self.max = maximum
+        self.count = count
+        self.filename = filename
+
+    def genrandom(self):
+        """Generates random data in output file."""
+        random.seed(self.seed)
+        with open(self.filename, 'w') as f:
+            for i in range(self.count):
+                f.write(str(random.randint(self.min, self.max)))
+                f.write('\n')
+
+
+def main():
+    """Main function, executed if this file is run."""
+    genrandforsorting(time.time()).genrandom()
+    return 0
+
+if __name__ == "__main__":
+    """Execute only if run as a script."""
+    main()
